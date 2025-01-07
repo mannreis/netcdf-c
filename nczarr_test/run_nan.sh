530b29c6d2ab52abc69c17a00dfd487d43b10d3a
Dennis Heimbigner <dennis.heimbigner@ieee.org>
2025-01-07 15:43:31 -0700

    resync with v3meta.tmp

--- nczarr_test/run_nan.sh
+++ nczarr_test/run_nan.sh
@@ -47,8 +47,6 @@ for t in tst_nans ; do
 export NCTRACING=10
    ${NCGEN} -4 -lb -o ${fileurl} ${cdl}/${ref}.cdl
    ${NCDUMP} ${headflag} ${specflag} -n ${ref} ${fileurl} > tmp_${t}.dmp
-unset NCTRACING
-${ZMD} -t float ${fileurl}
    # compare against expected
    diff -b -w ${expected}/${ref}.dmp ./tmp_${t}.dmp
    echo "*** SUCCEED: ${t}"
