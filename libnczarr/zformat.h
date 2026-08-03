b7c95d2ec9420ca4d0cead5aa207312e7206823c
Manuel Reis <reis@dkrz.de>
2026-08-03 12:17:41 +0200

    Unformat file

--- libnczarr/zformat.h
+++ libnczarr/zformat.h
@@ -3,6 +3,5 @@
 
 int NCZ_infer_zarr_format(NC_FILE_INFO_T *file);
 int NCZ_infer_nczarr_format(NC_FILE_INFO_T *file);
-int NCZ_get_map(NC_FILE_INFO_T *file, NCURI *uri, mode_t mode,
-                size64_t constraints, void *params, NCZMAP **mapp);
+int NCZ_get_map(NC_FILE_INFO_T*file,NCURI*uri,mode_t mode,size64_t constraints,void *params, NCZMAP**mapp);
 #endif
